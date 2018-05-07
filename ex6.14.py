ee206aeaec4e7b06cca4bc2995c32471dd834d2f
Unknown <p15148@students.cs.unipi.gr>
2018-05-07 23:53:49 +0300

    6.14 6.15 working

--- ex6.14.py
+++ ex6.14.py
@@ -1,15 +1,15 @@
-# 2 players nim game with 2 piles wins the one that can't pick nucleotides
+# 2 players nim game with 2 piles /wins the one that can't pick nucleotides according to the rule below
 # in every round the player chooses 2 from one pile and 1 from the other
 # player 1 plays with winning strategy
 # player 2 plays randomly
 import random
 import numpy as np
 
-n = 100  # pile A size
-m = 100  # pile B size
-pileA = []*n
-pileB = []*m
-R = np.zeros((n, m))  # strategy array 1 means winning position 0 means losing position
+n = 90  # pile A size
+m = 90  # pile B size
+pileA = [0]*n
+pileB = [0]*m
+R = np.zeros((n, m))  # strategy array 1 indicates winning position 0 indicates losing position
 
 
 def winning_strategy():
@@ -18,49 +18,79 @@ def winning_strategy():
         R[i][0] = 1
     for j in range(m):  # pile a has 0 nucleotides so no move available
         R[0][j] = 1
-    for i in range(2, n):  #
+    for i in range(2, n):  # only one possible move that always leads to win for the enemy
         R[i][1] = 0
-    for j in range(2, m):
+    for j in range(2, m):  # only one possible move that always leads to win for the enemy
         R[1][j] = 0
     for i in range(2, n):
         for j in range(2, m):
-            if R[i-2][j-1] == 1 and R[i-1][j-2] == 1:
+            if R[i-2][j-1] == 1 and R[i-1][j-2] == 1:  # if both possible moves lead to win pos for the enemy this is losing position
                 R[i][j] = 0
             else:
                 R[i][j] = 1
 
 
-def player_1():
-    if R[i-2][j-1] == 0:
-        # if this next move leads to losing position for enemy
-        # pile a -2  pile b-1
-        del pileA[i], pileA[i-1], pileB[i]
-    elif R[i-1][j-2] == 0:
-        # if this next move leads to losing position for enemy
-        # pile a -1 pile b-1
-        del pileA[i], pileB[i], pileB[i - 1]
+def player_1(i, j):
+    index_a = 0  # it is equal to the nucleotides player will delete from each pile
+    index_b = 0
+
+    if R[i-2][j-1] == 0:  # if next move leads to losing position for enemy delete according movement
+        del pileA[i], pileA[i-1], pileB[j]
+        index_a = 2
+        index_b = 1
+    elif R[i-1][j-2] == 0:  # if next move leads to losing position for enemy delete according movement
+        del pileA[i], pileB[j], pileB[j-1]
+        index_a = 1
+        index_b = 2
     else:
-        # if this move leads in winning position the enemy for both cases pick one
-        choice = random.uniform(0, 1)
+        choice = random.uniform(0, 1)  # if this move leads in winning position for the enemy in both cases the pick randomly
         if choice > 0.5:
-            # remove 2 from pile a remove 1 from pile b
-            del pileA[i], pileA[i - 1], pileB[i]
+            del pileA[i], pileA[i-1], pileB[j]
+            index_a = 2
+            index_b = 1
         else:
-            # remove 1 from pile a remove 2 from pile b
-            del pileA[i], pileB[i], pileB[i - 1]
+            del pileA[i], pileB[j], pileB[j - 1]
+            index_a = 1
+            index_b = 2
+    return index_a, index_b
 
 
-def player_2():
-    choice = random.uniform(0, 1)
-    if choice > 0.5:
-        print(choice)
-        del pileA[i], pileA[i - 1], pileB[i]
-        # remove 2 from pile a remove 1 from pile b
+def player_2(i, j):
+    index_a = 0
+    index_b = 0
+
+    choice = random.uniform(0, 1)  # pick a float between 0 and 1
+    if choice > 0.5:  # give equal chances for the possible moves
+        del pileA[i], pileA[i - 1], pileB[j]
+        index_a = 2
+        index_b = 1
     else:
-        print(choice)
-        del pileA[i], pileB[i], pileB[i - 1]
-        # remove 1 from pile a remove 2 from pile b
+        del pileA[i], pileB[j], pileB[j - 1]
+        index_a = 1
+        index_b = 2
+    return index_a, index_b
 
 
 winning_strategy()
-print(R)
+# print(R)
+
+index_pileA = n - 1
+index_pileB = m - 1
+
+
+while True:
+    if (index_pileA >= 2 and index_pileB >= 1) or (index_pileA >= 1 and index_pileB >= 2):
+        move = player_1(index_pileA, index_pileB)
+        index_pileA = index_pileA - move[0]
+        index_pileB = index_pileB - move[1]
+    else:
+        print("Player 1 won")
+        break
+    if (index_pileA >= 2 and index_pileB >= 1) or (index_pileA >= 1 and index_pileB >= 2):
+        move = player_2(index_pileA, index_pileB)
+        index_pileA = index_pileA - move[0]
+        index_pileB = index_pileB - move[1]
+    else:
+        print("Player 2 won")
+        break
+# gia mikres stives an o player 1 ksekinaei apo losing pos uparxei pithanothta na xasei
--- ex6.15.py
+++ ex6.15.py
@@ -1,17 +1,17 @@
-# 2 players nim game with 1 pile wins the one that deletes the last nucleotide
-# in every round the player chooses either one or two from the pile
+# 2 players nim game with 1 pile /wins the one that takes the last nucleotide
+# in every round each player chooses either one or two from the pile
 # player 1 plays with winning strategy
 # player 2 plays randomly
 import random
 import numpy as np
 
-n = 100  # pile size
-pile = []*n
-R = np.zeros(n)
+n = 88  # pile size
+pile = [0]*n  # nucleotide pile
+R = np.zeros(n)  # strategy array 1 indicates winning position 0 indicates losing position
 
 
 def winning_strategy():
-    R[0], R[1], R[2] = 0, 1, 1
+    R[0], R[1], R[2] = 0, 1, 1  # if pile has 1 or 2 nucleotides left he wins /0 loses
     for i in range(3, n):
         if R[i-1] == 1 and R[i-2] == 1:
             R[i] = 0
@@ -20,14 +20,15 @@ def winning_strategy():
 
 
 def player_1(i):
-    index_move = 0
-    if R[i] == 1 and R[i-1] == 1:
+    index_move = 0  # it is equal to the nucleotides player will delete
+
+    if R[i] == 1 and R[i-1] == 1:  # if current and next are winning pos delete 2 so opponent end up losing
         del pile[i], pile[i - 1]
         index_move = 2
-    elif R[i] == 1 and R[i-1] == 0:
+    elif R[i] == 1 and R[i-1] == 0:  # if current pos is winning and next losing delete 1
         del pile[i]
         index_move = 1
-    elif R[i] == 0:
+    elif R[i] == 0:  # else pick randomly (like player2)
         choice = random.uniform(0, 1)
         if choice > 0.5:
             del pile[i]
@@ -39,20 +40,31 @@ def player_1(i):
 
 
 def player_2(i):
-    choice = random.uniform(0, 1)
-    if choice > 0.5:
+    index_move = 0
+
+    choice = random.uniform(0, 1)  # pick a float between 0 and 1
+    if choice > 0.5:  # give equal chances for the possible moves
         del pile[i]
+        index_move = 1
     else:
         del pile[i], pile[i-1]
+        index_move = 2
+    return index_move
 
 
-winning_strategy()
-i = n
+winning_strategy()  # build strategy matrix
+# print(R)
+
+game_index = n - 1  # create index that updates with every round /points at last nucleotide of the list
+
 while True:
-    player_1(i)
-    # afairw to index proxwraw tosa vhmata
-    player_2(i)
-    i = i - 1
-
-
-print(R)
+    move = player_1(game_index)  # store the index_move
+    game_index = game_index - move  # update index
+    if game_index == 0:  # if no nucleotides left in the pile /means player did the last possible move
+        print('Player 1 Won')
+        break
+    move = player_2(game_index)
+    game_index = game_index - move
+    if game_index == 0:
+        print('Player 2 Won')
+        break
