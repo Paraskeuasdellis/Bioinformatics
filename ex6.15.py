82a047edb3b7cfdf7da72f2b7c4c35fa672dbb45
Unknown <p15148@students.cs.unipi.gr>
2018-05-23 23:38:52 +0300

    final fixes on 6.15

--- ex6.15.py
+++ ex6.15.py
@@ -20,19 +20,15 @@ def convert_data_to_list():
 
 
 def player_1():
-    if index % 3 == 2:  # player is at winning position and needs to delete two nucleotide to send enemy to losing pos
-        #del pile[index], pile[index - 1]
+    if index % 3 == 1:  # player is at winning position and needs to delete two nucleotide to send enemy to losing pos
         index_move = 2
-    elif index % 3 == 1:
-        #del pile[index], pile[index - 1]
+    elif index % 3 == 0:
         index_move = 1
-    elif index % 3 == 0:  # player is at losing position so he picks randomly
+    elif index % 3 == 2:  # player is at losing position so he picks randomly
         choice = random.uniform(0, 1)
         if choice > 0.5:
-            #del pile[index]
             index_move = 1
         else:
-            #del pile[index], pile[index - 1]
             index_move = 2
     return index_move
 
@@ -40,10 +36,8 @@ def player_1():
 def player_2():
     choice = random.uniform(0, 1)  # pick a float between 0 and 1
     if choice > 0.5:  # give equal chances for the possible moves
-        #del pile[index]
         index_move = 1
     else:
-        #del pile[index], pile[index - 1]
         index_move = 2
     return index_move
 
@@ -63,20 +57,21 @@ if n == 0:  # if there are no nucleotides
 
 
 while True:
+    if index < 0:  # if no nucleotides left in the pile /means player did the last possible move
+        print('Player 2 Won')
+        break
     move = player_1()  # store the index_move
     count = count + 1  # update counter after each move
     index = index - move  # update index
     data = np.vstack((data, (index, count)))  # update array to plot
-    if index == 0:  # if no nucleotides left in the pile /means player did the last possible move
+    if index < 0:
         print('Player 1 Won')
         break
     move = player_2()
     count = count + 1
     index = index - move
     data = np.vstack((data, (index, count)))
-    if index == 0:
-        print('Player 2 Won')
-        break
+
 
 # plot the game
 x, y = data.T
